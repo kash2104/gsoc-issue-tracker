a592bd0b1869a10d068636c4e3461f4aa8792fbf
kash2104 <kparikh1104@gmail.com>
2024-06-16 15:20:59 +0530

    home page styled.

--- app/OrgDetails/[organizationName]/page.tsx
+++ app/OrgDetails/[organizationName]/page.tsx
@@ -1,23 +1,35 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { useRouter, useSearchParams, useParams } from "next/navigation";
+import { useParams } from "next/navigation";
+import Banner from "@/app/components/Banner";
+import RepoCard from "@/app/components/RepoCard";
 
 export default function OrgDetails() {
-  const router = useRouter();
   const organizationName = useParams().organizationName;
   const [orgRepos, setOrgRepos] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
+  const [orgImage, setOrgImage] = useState<string | any>(null);
+  const [year, setYear] = useState<string | any>(null);
+  const [ownerDetails, setOwnerDetails] = useState<any>(null);
+  const [newOwnerDetails, setnewOwnerDetails] = useState<any>(null);
 
   useEffect(() => {
     const fetchOrgRepos = async () => {
       try {
         const response = await fetch(`/api/repos/${organizationName}`);
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
+        const newResponse = await fetch(
+          `https://api.github.com/users/${organizationName}`
+        );
+        if (!response.ok || !newResponse.ok) {
+          throw new Error("Network response was not ok");
         }
         const data = await response.json();
+        const newData = await newResponse.json();
         console.log(data);
+        setOwnerDetails(data.repos[0].owner);
+        setnewOwnerDetails(newData);
+        setOrgImage(data.repos[0].owner.avatar_url);
         setOrgRepos(data.repos);
       } catch (error) {
         console.error("Fetch error:", error);
@@ -30,17 +42,43 @@ export default function OrgDetails() {
   }, [organizationName]);
 
   if (loading) {
-    return <div className="spinner"></div>;
+    return (
+      <div className="flex flex-col items-center gap-2 justify-center mt-[20%]">
+        <div className="spinner"></div>
+        <div>Loading....</div>
+      </div>
+    );
   }
+
+  const reposWithOpenIssues = orgRepos.filter(
+    (repo) => repo.open_issues_count > 0
+  );
 
   return (
     <div>
-      <h1>{organizationName}</h1>
-      <ul>
-        {orgRepos.map((repo) => (
-          <li key={repo.id}>{repo.name}</li>
-        ))}
-      </ul>
+      <Banner
+        image={orgImage}
+        name={organizationName}
+        website={newOwnerDetails.blog ?? "No website found!!"}
+        followers={newOwnerDetails.followers}
+        email={newOwnerDetails.email}
+        repos={reposWithOpenIssues.length}
+      />
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
+        {orgRepos.length !== 0 &&
+          reposWithOpenIssues.map((repo) => (
+            <div key={repo.id}>
+              <RepoCard
+                name={repo.name}
+                description={repo.description}
+                link={`https://github.com/${organizationName}/${repo.name}/issues?q=is%3Aissue+is%3Aopen+no%3Aassignee`}
+                issues={repo.open_issues_count}
+                createdAt={repo.created_at}
+              />
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
